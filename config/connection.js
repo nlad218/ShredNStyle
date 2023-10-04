12fa20b82d536a45d29ad723d0455606c02cccde
DallasThompson <duhtennis1@gmail.com>
2023-10-04 12:06:14 -0800

    adding some seed

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1', // Locolhost for windows // 127.0.0.1 for mac
-    dialect: 'mysql',
+    host: "localhost", // Locolhost for windows // 127.0.0.1 for mac
+    dialect: "mysql",
     port: 3306,
   }
 );
