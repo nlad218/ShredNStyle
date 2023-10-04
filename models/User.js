af7708a2ec3087ec5c276632b33e7c3a3f224b76
Jake Zook <jakezook01@gmail.com>
2023-10-04 10:59:49 -0600

    Added validation for email and password.

--- models/User.js
+++ models/User.js
@@ -1,36 +1,56 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
-const User = sequelize.define('User', {
-  ID: {
-    type: DataTypes.INT,
-    allowNull: false,
-    unique: true,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  }
-},
-  {
-    // Link to database connection
-    sequelize,
-    timestamps: false,
-    underscored: true,
-    modelName: 'book'
-  }
+class User extends Model {
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password);
+	}
+}
+
+const User = sequelize.define(
+	"User",
+	{
+		ID: {
+			type: DataTypes.INT,
+			allowNull: false,
+			unique: true,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+			},
+		},
+		userName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [8],
+			},
+		},
+	},
+	{
+		hooks: {
+			async beforeCreate(newUserData) {
+				newUserData.password = await bcrypt.hash(newUserData.password, 10);
+				return newUserData;
+			},
+		},
+		sequelize,
+		timestamps: false,
+		underscored: true,
+		modelName: "book",
+	}
 );
 
 module.exports = User;
