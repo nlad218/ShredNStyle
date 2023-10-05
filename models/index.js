40e60ffd929936368bf5b78adcc2556e4feffb59
Jake Zook <jakezook01@gmail.com>
2023-10-05 11:35:21 -0600

    Adjusted images on products page and added a mask layer.

--- models/index.js
+++ models/index.js
@@ -1,29 +1,29 @@
 const Product = require("./product");
 const User = require("./User");
 const Category = require("./categories");
-const Order = require("./orders");
-const OrderProduct = require("./orderproduct");
+// const Order = require("./orders");
+// const OrderProduct = require("./orderproduct");
 
 Product.belongsTo(Category, { foreignKey: "category_id" });
 
 Category.hasMany(Product);
 
-Order.belongsToMany(Product, {
-  foreignKey: "order_id",
-  through: OrderProduct,
-});
+// Order.belongsToMany(Product, {
+//   foreignKey: "order_id",
+//   through: OrderProduct,
+// });
 
-Product.belongsToMany(Order, {
-  foreignKey: "product_id",
-  through: OrderProduct,
-});
+// Product.belongsToMany(Order, {
+//   foreignKey: "product_id",
+//   through: OrderProduct,
+// });
 
-User.hasMany(Order, { foreignKey: "user_id" });
+// User.hasMany(Order, { foreignKey: "user_id" });
 
-Order.belongsTo(User);
+// Order.belongsTo(User);
 
 module.exports = {
-  Product,
-  Category,
-  User,
+	Product,
+	Category,
+	User,
 };
