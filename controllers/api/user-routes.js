0e4547008970beb65338063c4ccff2333b8829ab
Jake Zook <jakezook01@gmail.com>
2023-10-05 14:51:25 -0600

    Added icons to login modal.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,85 +3,85 @@ const { User } = require("../../models");
 const { sendEmail } = require("../../utils/sendemail");
 // Create a new user
 router.post("/", async (req, res) => {
-  try {
-    // Check if the email already exists in the database
-    const existingUser = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+	try {
+		// Check if the email already exists in the database
+		const existingUser = await User.findOne({
+			where: {
+				email: req.body.email,
+			},
+		});
 
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already in use" });
-    }
+		if (existingUser) {
+			return res.status(400).json({ message: "Email already in use" });
+		}
 
-    // Create a new user
-    const newUser = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+		// Create a new user
+		const newUser = await User.create({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password,
+		});
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+		req.session.save(() => {
+			req.session.loggedIn = true;
 
-      sendEmail(newUser.email);
+			sendEmail(newUser.email);
 
-      res.status(201).json(newUser);
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json(error);
-  }
+			res.status(201).json(newUser);
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json(error);
+	}
 });
 
 // Login
 router.post("/login", async (req, res) => {
-  try {
-    const dbUserData = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+	try {
+		const dbUserData = await User.findOne({
+			where: {
+				email: req.body.email,
+			},
+		});
 
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password. Please try again!" });
-      return;
-    }
+		if (!dbUserData) {
+			res
+				.status(400)
+				.json({ message: "Incorrect email or password. Please try again!" });
+			return;
+		}
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
+		const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password. Please try again!" });
-      return;
-    }
+		if (!validPassword) {
+			res
+				.status(400)
+				.json({ message: "Incorrect email or password. Please try again!" });
+			return;
+		}
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+		req.session.save(() => {
+			req.session.loggedIn = true;
 
-      res
-        .status(200)
-        .json({ user: dbUserData, message: "You are now logged in!" });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+			res
+				.status(200)
+				.json({ user: dbUserData, message: "You are now logged in!" });
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
 // Logout
 router.post("/logout", (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+	if (req.session.loggedIn) {
+		req.session.destroy(() => {
+			res.status(204).end();
+		});
+	} else {
+		res.status(404).end();
+	}
 });
 
 module.exports = router;
