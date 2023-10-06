de5a5a220c4aac188d00cd278f552b694e170a57
nlad218 <nlad218@gmail.com>
2023-10-06 11:43:36 -0400

    push to get code up to date

--- controllers/viewPage.js
+++ controllers/viewPage.js
@@ -4,61 +4,61 @@ const { Category } = require("../models");
 
 // CREATE new user
 router.get("/", async (req, res) => {
-	try {
-		const categoriesData = await Category.findAll();
-		const categories = categoriesData.map((obj) => obj.get({ plain: true }));
+  try {
+    const categoriesData = await Category.findAll();
+    const categories = categoriesData.map((obj) => obj.get({ plain: true }));
 
-		res.render("home", { categories });
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+    res.render("home", { categories });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/cart", async (req, res) => {
-	try {
-		res.render("cart");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("cart");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/products", async (req, res) => {
-	try {
-		res.render("products");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("products");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/passes", async (req, res) => {
-	try {
-		res.render("passes");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("passes");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-
 router.get("/resort", async (req, res) => {
-	try {
-		res.render("resortInfo");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("resortInfo");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("*", async (req, res) => {
-	try {
-		res.render("404");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("404");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 router.get("/productPage", async (req, res) => {
   try {
@@ -68,7 +68,5 @@ router.get("/productPage", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-
 
 module.exports = router;
