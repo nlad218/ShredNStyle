fd536934c4a2d111e9677d43efd547fa50c4ff85
nlad218 <nlad218@gmail.com>
2023-10-06 11:12:33 -0400

    added code to cart handlebars and code for retreiving from local storage

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,10 +1,24 @@
+// const cart = JSON.parse(localStorage.getItem("cart")) || [];
+// const cartEl = document.querySelector(".cart");
+// for (let i = 0; i < cart.length; i++) {
+//   console.log(cart[i]);
+// }
+
+const cartItemTemplateSource =
+  document.getElementById("cart-item-template").innerHTML;
+const cartItemTemplate = Handlebars.compile(cartItemTemplateSource);
+
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
-const cartEl = document.querySelector(".cart");
+const cartItemsContainer = document.getElementById("cart-items");
+
 for (let i = 0; i < cart.length; i++) {
   const item = cart[i];
-  console.log(item);
 
-  const listItem = document.createElement("li");
-  listItem.textContent = `${item.name} - Price: $${item.price}`;
-  cartEl.appendChild(listItem);
+  const context = {
+    name: item.name,
+    price: item.price,
+  };
+  const cartItemHTML = cartItemTemplate(context);
+
+  cartItemsContainer.innerHTML += cartItemHTML;
 }
