17c361cfb4a1e15a8eee7456bd0a2fa2b99d4172
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-10-06 20:24:59 -0600

    fix email bug

--- controllers/viewPage.js
+++ controllers/viewPage.js
@@ -7,93 +7,99 @@ router.get("/", async (req, res) => {
     const categoriesData = await Category.findAll();
     const categories = categoriesData.map((obj) => obj.get({ plain: true }));
     const productsData = await Product.findAll();
-    const products = productsData.map((obj) => obj.get({ plain: true })).slice(0,6);
-    
+    const products = productsData
+      .map((obj) => obj.get({ plain: true }))
+      .slice(0, 6);
 
     res.render("home", { categories, products });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-  
 });
 
 router.get("/cart", async (req, res) => {
-	try {
-		res.render("cart");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("cart");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get("/products", async (req, res) => {
-	try {
-		res.render("products");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+router.get("/products/:category_id", async (req, res) => {
+  try {
+    const productsData = await Product.findAll({
+      where: {
+        id: req.params.category_id,
+      },
+    });
+
+    res.render("products");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/passes", async (req, res) => {
-	try {
-		res.render("passes");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("passes");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/resort", async (req, res) => {
-	try {
-		//need to grab states from database (resort Info table)
-		//setup a const to bring in the model
-		//logged date
-		//serielize data
-		const resortData = await ResortInfo.findAll();
-		// console.log(resortData);
-		const resorts = resortData.map((resort) => resort.get({ plain: true }));
-		//plain: true is serilizing every data passed through it
-		//now the data has been parced through
+  try {
+    //need to grab states from database (resort Info table)
+    //setup a const to bring in the model
+    //logged date
+    //serielize data
+    const resortData = await ResortInfo.findAll();
+    // console.log(resortData);
+    const resorts = resortData.map((resort) => resort.get({ plain: true }));
+    //plain: true is serilizing every data passed through it
+    //now the data has been parced through
 
-		// res.render("resortInfo");
-		// console.log(resorts);
-		// resortInfo is reffering to the resortInfo Handlebars in the VIEWS directory not the partials
-		//resorts is the new array of the serialized objects
-		res.render("resortInfo", { resorts });
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+    // res.render("resortInfo");
+    // console.log(resorts);
+    // resortInfo is reffering to the resortInfo Handlebars in the VIEWS directory not the partials
+    //resorts is the new array of the serialized objects
+    res.render("resortInfo", { resorts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/resort/:state", async (req, res) => {
-	const stateValue = req.params.state;
-	console.log(`THIS IS LINE 70: ${stateValue}`);
-	//now grab and send all the data that has the statevalue ive given in the query parameter
-	const resortInfo = await ResortInfo.findAll({
-		where: {
-			state: stateValue,
-		},
-	});
-	// console.log(resortInfo);
-	const stateResorts = resortInfo.map((resort) => resort.get({ plain: true }));
-	console.log(stateResorts);
-	res.render("stateResorts", { stateResorts });
+  const stateValue = req.params.state;
+  console.log(`THIS IS LINE 70: ${stateValue}`);
+  //now grab and send all the data that has the statevalue ive given in the query parameter
+  const resortInfo = await ResortInfo.findAll({
+    where: {
+      state: stateValue,
+    },
+  });
+  // console.log(resortInfo);
+  const stateResorts = resortInfo.map((resort) => resort.get({ plain: true }));
+  console.log(stateResorts);
+  res.render("stateResorts", { stateResorts });
 });
 
 router.get("/productPage/:id", async (req, res) => {
   try {
     const productsData = await Product.findOne({
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     });
 
-    const product = productsData.get({plain: true});
+    const product = productsData.get({ plain: true });
     console.log(product);
-    res.render("productPage", {product});
+    res.render("productPage", { product });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -101,11 +107,11 @@ router.get("/productPage/:id", async (req, res) => {
 });
 
 router.get("*", async (req, res) => {
-	try {
-		res.render("404");
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
+  try {
+    res.render("404");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 module.exports = router;
