4bd8d6383d25eebda781c10ae86f96eace561455
JMADA257 <jadams1812@hotmail.com>
2023-10-09 13:30:51 -0600

    Fixed a bug

--- seeds/reviewsSeed.js
+++ seeds/reviewsSeed.js
@@ -1,15 +1,15 @@
-const { Reviews } = require("../models");
+// const { Reviews } = require("../models");
 
-const reviewData = [
-  {
-    review: "I dont know does this work?",
-  },
-  {
-    name: "Jordan Adams",
-  },
-  { stars: 3 },
-];
+// const reviewData = [
+//   {
+//     review: "I dont know does this work?",
+//   },
+//   {
+//     name: "Jordan Adams",
+//   },
+//   { stars: 3 },
+// ];
 
-const seedReview = () => Reviews.bulkCreate(reviewData);
+// const seedReview = () => Reviews.bulkCreate(reviewData);
 
-module.exports = seedReview;
+// module.exports = seedReview;
