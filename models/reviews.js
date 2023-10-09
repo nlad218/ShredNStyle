107d2daf8a458d36cd17d5bf5cdd580810ff5d53
JMADA257 <jadams1812@hotmail.com>
2023-10-09 13:29:38 -0600

    Fixed sequelize error

--- models/reviews.js
+++ models/reviews.js
@@ -1,32 +1,37 @@
+
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 class Reviews extends Model {}
 
-Reviews.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Reviews.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  review: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  stars: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "reviews",
-});
+  {
+    sequelize, // Pass the Sequelize instance here
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "reviews",
+  }
+);
 
 module.exports = Reviews;
