c8c076aaf05dc4af90b3639fa444f86bd28a9b13
JMADA257 <jadams1812@hotmail.com>
2023-10-08 22:14:07 -0600

    Added basic fetch for the weather api

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,10 +7,5 @@ module.exports = {
       new Date(date).getFullYear() + 5
     }`;
   },
-  get_weather: (lat, long) => {
-    console.log(lat, long);
-    //another function for calling weather within the module.exports, this function will take the long lat and make an api call to get the weather data 
-}
-
+  //another function for calling weather within the module.exports, this function will take the long lat and make an api call to get the weather data
 };
-
