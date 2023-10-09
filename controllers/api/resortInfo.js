57f0296abb51e3c435eff73d18e95db15b6e1edf
JMADA257 <jadams1812@hotmail.com>
2023-10-08 19:09:24 -0600

    Added an extra route for getting resorts by id just in case

--- controllers/api/resortInfo.js
+++ controllers/api/resortInfo.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
 const { ResortInfo } = require("../../models");
 
-router.get("/resortInfo", async (req, res) => {
+router.get("/resorts", async (req, res) => {
   try {
     const resortData = await ResortInfo.findAll();
+    res.status(200).json(resortData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/resorts/:id", async (req, res) => {
+  try {
+    const resortData = await ResortInfo.findByPk(req.params.id);
     res.status(200).json(resortData);
   } catch (err) {
     res.status(500).json(err);
