114abcc8f97b0cd811c852971e141f561f45f8ee
JMADA257 <jadams1812@hotmail.com>
2023-10-09 13:10:05 -0600

    Added model boilerplate for reviews

--- models/reviews.js
+++ models/reviews.js
@@ -3,6 +3,30 @@ const sequelize = require("../config/connection");
 
 class Reviews extends Model {}
 
-Reviews.init({});
+Reviews.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  review: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  stars: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "reviews",
+});
 
 module.exports = Reviews;
