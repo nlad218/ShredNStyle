1e76ef27157e9ae225a48c61c851d7d16e2e5ee6
JMADA257 <jadams1812@hotmail.com>
2023-10-10 10:17:54 -0600

    Fixed some buggs

--- controllers/viewPage.js
+++ controllers/viewPage.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { Category, Product, ResortInfo } = require("../models");
 const { Op } = require("sequelize");
 const { Category, Product, ResortInfo, UserProduct } = require("../models");
 
